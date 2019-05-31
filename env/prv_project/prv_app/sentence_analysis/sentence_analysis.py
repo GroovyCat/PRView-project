e508a7ac3870ed3f29210a273e7a4d875b1c2588
박 문영 <44114405+parkmy123@users.noreply.github.com>
2019-05-31 17:08:38 +0900

    주석, 변수명 등 수정
    좀 더 알아보기 쉽게 하기 위한 정리작업

--- env/prv_project/prv_app/sentence_analysis/sentence_analysis.py
+++ env/prv_project/prv_app/sentence_analysis/sentence_analysis.py
@@ -8,8 +8,8 @@ from collections import Counter
 def get_tags(text, noun_count):
     spliter = Okt()
     nouns = spliter.nouns(text) # nouns 함수를 통해서 text에서 명사만 분리/추출
-    count = Counter(nouns) # Counter객체를 생성하고 참조변수 nouns할당
-    return_list = [] # 명사, 빈도를 저장할 변수
+    count = Counter(nouns) # Counter 객체를 생성하고 참조변수 nouns할당
+    return_list = [] # 명사, 빈도 딕셔너리를 저장할 리스트
 
     # most_common 메소드는 정수를 입력받아 객체 안의 명사중 빈도 수가
     # 큰 명사부터 순서대로 입력받은 정수 갯수만큼 저장되어있는 객체 반환
@@ -20,20 +20,20 @@ def get_tags(text, noun_count):
     return return_list
 
 def main():
-    text_file_name = "input.txt" # 분석할 문장이 저장된 파일명
-    output_file_name = "count.txt" # 분석된 명사들을 저장하기 위한 파일명
-    noun_count = 30 # 최대 많은 빈도수 부터 n개 명사 추출
+    input_file = "input.txt" # 분석할 문장이 저장된 파일명
+    result_file_ = "count.txt" # 분석된 명사들과 빈도를 저장하기 위한 파일명
+    noun_count = 100 # noun_count 값 많큼의 명사를 추출
 
-    open_text_file = open(text_file_name, 'r', -1, "utf-8") # 분석할 파일 열기 
-    text = open_text_file.read() # 해당 파일의 내용 읽기
+    open_input_file = open(input_file, 'r', -1, "utf-8") # 분석할 input.txt 파일 열기 
+    text = open_input_file.read() # 해당 파일의 내용 읽기
     tags = get_tags(text, noun_count) # get_tags 함수 실행
-    open_text_file.close() # 파일 닫기
+    open_input_file.close() # 파일 닫기
 
-    open_output_file = open(output_file_name, 'w', -1, "utf-8") # 결과로 쓰일 count.txt 열기
+    open_output_file = open(result_file, 'w', -1, "utf-8") # 결과로 쓰일 count.txt 열기
     for tag in tags:
         noun = tag['tag']
         count = tag['count']
-        open_output_file.write('{} {}\n'.format(noun, count)) # {명사}, {횟수}의 형태로 결과 파일에 저장
+        open_output_file.write('{} {}\n'.format(noun, count)) # (명사 횟수)의 형태로 결과 파일에 저장
     open_output_file.close() # 파일 닫기
  
 if __name__ == '__main__':
