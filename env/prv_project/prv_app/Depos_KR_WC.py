603a724caca95b54a1d93a34b545a529b6efdaa5
GroovyCat <kodeep11@gmail.com>
2019-06-09 01:53:09 +0900

    파일 경로 재 설정

--- env/prv_project/prv_app/Depos_KR_WC.py
+++ env/prv_project/prv_app/Depos_KR_WC.py
@@ -5,31 +5,24 @@ import matplotlib.pyplot as plt # 워드 클라우드 이미지 생성을 위한
 from wordcloud import WordCloud
 import random
 
-def color_func(word, font_size, position, orientation, random_state=None,
-                    **kwargs):
+def color_func(word, font_size, position, orientation, random_state=None, **kwargs):
     return "hsl(0, 100%%, %d%%)" % random.randint(60, 100)
 
-dislike_mask = np.array(Image.open("dislike.png"))#싫어요 mask
-font_path = './Maplestory_Bold.ttf' #폰트 path
+    dislike_mask = np.array(Image.open("C:/Python_basic/env/prv_project/prv_app/static/img/dislike.png"))#싫어요 mask
+    font_path = './Maplestory_Bold.ttf' #폰트 path
 
-wordcloud = WordCloud(
-    font_path = font_path,
-    width = 800,
-    height = 800,
-    background_color="white",#바탕색
-    contour_width=1,#테두리 굵기
-    contour_color='red',#테두리색
-    mask = dislike_mask #마스크 설정
-)
-
-wordcloud = wordcloud.generate_from_frequencies(word_list)#워드클라우드 생성
-
-
-fig = plt.figure(figsize=(10, 10))
-plt.imshow(wordcloud.recolor(color_func=color_func, random_state=3),
-           interpolation="bilinear")
-#plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
-plt.show()#없애도 되는 코드 보여주기
-fig.savefig('dislike_result.png')# 저장
+    wordcloud = WordCloud(
+        font_path = font_path,
+        width = 800,
+        height = 800,
+        background_color="white",#바탕색
+        contour_width=1,#테두리 굵기
+        contour_color='red',#테두리색
+        mask = dislike_mask #마스크 설정
+    )
+    wordcloud = wordcloud.generate_from_frequencies(word_list)#워드클라우드 생성
+    fig = plt.figure(figsize=(10, 10))
+    plt.imshow(wordcloud.recolor(color_func=color_func, random_state=3),interpolation="bilinear")
+    plt.axis("off")
+    fig.savefig('C:/Python_basic/env/prv_project/prv_app/static/img_url_neg/url_neg.png')# 저장
 
--- env/prv_project/prv_app/Pos_KR_WC.py
+++ env/prv_project/prv_app/Pos_KR_WC.py
@@ -1,16 +1,16 @@
 from PIL import Image
 import numpy as np
-
+from . import splitSent
 import matplotlib.pyplot as plt # 워드 클라우드 이미지 생성을 위한 import
 from wordcloud import WordCloud
 import random
 
-def color_func(word, font_size, position, orientation, random_state=None,
-                    **kwargs):
+def color_func(word, font_size, position, orientation, random_state=None, **kwargs):
     return "hsl(955, 100%%, %d%%)" % random.randint(40, 100)#"hsl(색, 다양성, )" % random.radint(밝기,)
+    
+splitSent.get_tags()
 
-
-like_mask = np.array(Image.open("like.png"))#좋아요 mask
+like_mask = np.array(Image.open("C:/Python_basic/env/prv_project/prv_app/static/img/like.png"))#좋아요 mask
 font_path = './Maplestory_Bold.ttf' #폰트 path
 
 wordcloud = WordCloud(
@@ -22,13 +22,8 @@ wordcloud = WordCloud(
     contour_color='steelblue',#테두리
     mask = like_mask #마스크 설정
 )
-
 wordcloud = wordcloud.generate_from_frequencies(word_list)#워드클라우드 생성
-
 fig = plt.figure(figsize=(10, 10))
-plt.imshow(wordcloud.recolor(color_func=color_func, random_state=3),
-           interpolation="bilinear")
-#plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("
-plt.show()#없애도 되는 코드 보여주기
-fig.savefig('like_result.png')# 저장
+plt.imshow(wordcloud.recolor(color_func=color_func, random_state=3), interpolation="bilinear")
+plt.axis("off")
+fig.savefig('C:/Python_basic/env/prv_project/prv_app/static/img_url_pos/url_pos.png')# 저장
