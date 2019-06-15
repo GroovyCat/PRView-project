4021949f5e5740ff110b2c25a67cee6ad208f5c3
s0w0s <jangwooseng97@gmail.com>
2019-06-16 08:36:02 +0900

    1~10p 전체 내용 설명 추가 34p, 132p time함수를 이용해 전체 동작시간 체크 36~43 헤드리스모드 추가 [주석 처리 부분] 나머지 일부 내용 정리 및 라인 변경

--- env/prv_project/prv_app/shoppingMallCrawler.py
+++ env/prv_project/prv_app/shoppingMallCrawler.py
@@ -1,71 +1,97 @@
+'''
+
+<url을 입력 받아 구매후기(전체, 긍정, 부정)으로 나누어 텍스트를 생성하는 프로그램> 
+selenium 함수의 기능과 BeautifulSoup을 이용하여 페이지를 이동하며 원하는 값을 스크레핑 하고 방법으로는
+페이지를 이동하며 각 페이지의 속성 값들을 읽어 판단하여 진행하고 페이지의 타입을 int형 str형으로 변환하여 
+판단값으로 사용됩니다. 페이지를 selenium으로 이동하며 페이지를 BeautifulSoup으로 페이지 내용
+을 생성하여 전체,긍정,부정 데이터 리스트를 생성하고 그값을 통해 텍스트에 저장하는 프로그램입니다.
+
+'''
+
 # 1. 웹크롤링에 필요한 모듈 호출
-from selenium import webdriver 
+from selenium import webdriver #  웹크롤링을 위한 웹드라이버
+from bs4 import BeautifulSoup # 웹페이지 내용구조 해석
+# selenium 마우스 컨트롤 기능 ex control + click 
 from selenium.webdriver.common.action_chains import ActionChains  #contril click in selenium
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup # 웹페이지 내용구조 해석
-from time import sleep
-from . import splitSent
+from time import sleep,time     #작동시간 체크, sleep기능 사용
+#서버 접근 허용을 위해 사용 
 from urllib.request import urlopen 
 import requests
+#
+from . import splitSent
 import os
 
+
+#문자열중 숫자 찾는 함수
 def findnum(strnum):
     for i in range(0, len(strnum)) :
         if strnum[i].isdigit() == True :
             return strnum[i]
-#문자열중 숫자 찾는 함수
+
 
 def search_shop_review(URL):
+    start_time = time() #시간 함수 시작시간
     url = URL
-    #print(URL)
+
+    #헤드 리스 모드 사용할시  (42번 라인 지우고 주석 헤제) 
+    #options = webdriver.ChromeOptions() #크롬 옵션
+    #options.add_argument('headless')    #headless 모드
+    #options.add_argument('window-size=1920*1080')
+    #options.add_argument("disable-gpu")
+    #options.add_argument("--disable-gpu") #오류가 생길시
+    #driver = webdriver.Chrome('C:/Python_basic/env/prv_project/prv_app/chromedriver', chrome_options=options)
 
     driver = webdriver.Chrome('C:/Python_basic/env/prv_project/prv_app/chromedriver')
-    #driver = webdriver.PhantomJS('/Users\/kddn/Documents/crawling/phantomjs-2.1.1-windows/bin/phantomjs') #판다스 사용할 경우 사용
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(2)
  
     # url에 접근한다.
     driver.get(url)
     #driver.find_element_by_xpath("//a[@data-maintarget='#vip_tab_comment']").click() #xpath 테스트
-    driver.find_element_by_id("tap_moving_2").click()  
+    driver.find_element_by_id("tap_moving_2").click()   #구매 후기 위치 클릭
     sleep(1)
     
-    # 3. 댓글 페이지 html 구조 긁어오기
+    # 댓글 페이지 html 구조 긁어오기
     page = driver.page_source # 페이지의 elements모두 가져오기
     source = BeautifulSoup(page,'html.parser', from_encoding='utf-8') # 한글이 있기때문에 encoding 해줌  #[from_encoding='utf-8생략 가능]
     #print(source)
     
     # 페이지수 읽어오기 
-    try:#ok
+    try:
         page_num = source.find('div',{'class':'box__page-jump'}).find('em',{'class':'text'}).get_text()    #페이지 부분을 str 형태로 변환
         page_num=int(page_num) #str 을 int형으로 변환 
 
-    except:
-        page_num=1;#1페이지 일경우 1로 지정
+    except: #1페이지 이상 없을 경우 실행문
+        page_num=1;# 1페이지 일경우 1로 지정
+    #print(page_num)
     
-    print(page_num)
-    
+    #전체 긍정 부정 리스트 생성
     all_reviews=[]
     positive_reviews=[]
     negative_reviews=[]
     
+    #긍정 부정 점수 // defualt 값 입력을 받아 사용할수는 있으나 개발 코스트로 인해 defult로 사용했읍니다.
     positive = 4    #defualt 값
     negative = 3    #defualt 값
     
-    #페이지 수만큼 반복
-    try:
+    #리뷰 내용이 없을시 오류해결위한 try문
+    try:  #페이지 수만큼 반복
         for numPage in range(1,page_num):
-            # 4. 네티즌 댓글부분(태그 , {속성명: 속성값})   
+            # 현제 페이지 텍스트 page_source 와 BeautifulSoup 함수로 생성
             page = driver.page_source
             source = BeautifulSoup(page,'html.parser',from_encoding='utf-8')
 
-            all_review = source.find('ul',{'clss','list__review'}).findAll('div',{'class':'box__content'})     #내용 추출
+            # 현제 페이지 리뷰 부분 BeautifulSoup 리스트 생성
+            all_review = source.find('ul',{'clss','list__review'}).findAll('div',{'class':'box__content'})     #내용 추출 전체 리뷰.각 리뷰데이터
             #print(all_review)
             sleep(1)
 
-            #전체 긍정 부정 리스트 추가
+            #위 추출 내용중 전체 긍정 부정 리스트 추가 반복문
             for review in all_review:
-                all_reviews.append(review.find('p',{'class':'text'}).get_text().strip().replace('\n','').replace('\t','').replace('\r',''))
+                #전체 리뷰 리스트에 추가
+                all_reviews.append(review.find('p',{'class':'text'}).get_text().strip().replace('\n','').replace('\t','').replace('\r','')) #get_text 이상 부분은 텍스트로 변환 + 전처리
 
+                #별점내용 읽어들여 숫자 값만 뽑아서 int형으로 변환
                 star = review.find('span',{'class':'for-a11y'}).text  #변수에 별점 내용 저장 
                 star = int(findnum(star)) #내용중 findnum함수를 이용해 숫자를 찾아낸후 그것은 int형으로 저장
                 #print(type(star))
@@ -97,6 +123,10 @@ def search_shop_review(URL):
     for review in negative_reviews:
         file_n.write(review+'\n')
 
+    #텍스트 파일 생성후 파일 종료
     file_a.close()
     file_p.close()
     file_n.close()
+    
+    #전체 작동 시간 표시
+    print("실행 시간 : %s초" % (time() - start_time))
